ebff61785f78a1d4a5e4a618985fb3a65ccb12f8
CyrilStrone <cyrilstrone@gmail.com>
2023-09-19 14:24:32 +0700

    Add gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
+  <HashRouter basename='/cyril-strone'>
     <App />
   </HashRouter>,
 );
